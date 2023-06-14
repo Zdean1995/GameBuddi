0bbbabad041af10b1e3224a19957a4a520d2b816
Zachary <zdean1995@gmail.com>
2023-06-13 23:32:04 -0400

    Converted app to Mvvm including adding dependecy on communitytoolkit.mvvm and adding appropriate folders.  Created three models, one for users, one for reviews posted by those users, and one for comments on those reviews.  The models will be represented by tables on azure.  They will be connected via forgein keys.  The ids will be incrementally generated . Games model will be added later.

---
+++ GameBuddi/Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBuddi.Models
+{
+    /*
+     * The reviews for the games.  These will be on the games page and will be displayed with their own reputation and comments.  The review will
+     * have the posters name which will connect to their page.
+     */
+    internal class Review
+    {
+        public int Id { get; set; }
+        public string GameID { get; set; }
+        public int PosterId { get; set; }
+        public DateTime PostDate { get; set; } = DateTime.Now;
+        public double Score { get; set; }
+        public string ReviewTitle { get; set; }
+        public string ReviewContent { get; set; }
+        public int ReviewReputation { get; set; }
+    }
+}
---
+++ GameBuddi/Models/ReviewComment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBuddi.Models
+{
+    /*
+     * The comments for the reviews.  They will be displayed under the reviews and have their own scores.  
+     * The commenters account can be accessed by their account name on the comment.
+     */
+    internal class ReviewComment
+    {
+        public int Id {  get; set; }
+        public int ReviewId { get; set; }
+        public int PosterId { get; set; }
+        public string Content { get; set; }
+        public int CommentScore { get; set; }
+    }
+}
