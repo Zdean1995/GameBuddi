7de87dc89ff64272944a799e6a5e7f6bc4cfb532
Zachary <zdean1995@gmail.com>
2023-07-06 23:47:17 -0400

    Implemented IGDB wrapper.  Still need to check if this is cheating since it has models for the api making ours redundent.  Added IGDBManager as a middle man between the api and the issues commands to is using the IGDBClient object provided by the IGDB wrapper.  This has not been tested yet.  There are some basic commands based on the templates provided by IGDB.  Also removed some default code in the mainpage because it was causing errors.

---
+++ GameBuddi/Services/IGDBManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IGDB;
+using IGDB.Models;
+
+namespace GameBuddi.Data
+{
+    public static class IGDBManager
+    {
+        static readonly string IGDB_CLIENT_ID = "42ogbeoxpizxz5drqmx94l6dsqg5zw";
+        static readonly string IGDB_SECRET_ID = "hljej83qs386r3jedy594pe59r1vps";
+
+        static IGDBClient client = new IGDBClient(IGDB_CLIENT_ID, IGDB_SECRET_ID);
+
+        public static async Task<Game> GetGame(string id)
+        {
+            var games = await client.QueryAsync<Game>(IGDBClient.Endpoints.Games, query: $"fields *; where id = {id};");
+            return games.First();
+        }
+
+        public static async Task<Game[]> GetGames(string start)
+        {
+            return await client.QueryAsync<Game>(IGDBClient.Endpoints.Games, query: "fields name; limit 100;");
+
+        }
+    }
+}
